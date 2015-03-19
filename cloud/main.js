4c4c99827989206b2b2d97fc7108d613292b6dc3
Diego Haz <hazdiego@gmail.com>
2015-03-19 13:07:26 -0300

    Define initial services

---
+++ cloud/main.js
@@ -0,0 +1,191 @@
+var _ = require('underscore');
+var names = require('cloud/names.js');
+
+/**
+ * Validate user and set defaults
+ *
+ * @todo Validate feeling and nickname
+ */
+Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+  var nickname = request.object.get('nickname');
+  var feeling = request.object.get('feeling');
+
+  // Defaults
+  request.object.get('setup')     || request.object.set('setup', false);
+  request.object.get('echoed')    || request.object.set('echoed', []);
+  request.object.get('commented') || request.object.set('commented', []);
+  request.object.get('removed')   || request.object.set('removed', []);
+  request.object.get('following') || request.object.set('following', []);
+  request.object.get('language')  || request.object.set('language', 'en');
+
+  response.success();
+});
+
+/**
+ * Get adjectives
+ *
+ * @param {string} [language]
+ *
+ * @response {Object[]} List of feelings with array of adjectives
+ */
+Parse.Cloud.define('getAdjectives', function(request, response) {
+  // Params
+  var lang = request.params.language || request.user.get('language') || 'en';
+
+  if (lang in names.adjectives) {
+    response.success(names.adjectives[lang]);
+  } else {
+    response.error('Invalid language');
+  }
+});
+
+/**
+ * Get nicknames
+ *
+ * @todo Validate feeling
+ *
+ * @param {string} [language]
+ * @param {string} [feeling]
+ * @param {int} [limit=1] Number of nicknames to return
+ *
+ * @response {string[]} List of nicknames
+ */
+Parse.Cloud.define('getNicknames', function(request, response) {
+  // Params
+  var lang = request.params.language || request.user.get('language') || 'en';
+  var feeling = request.params.feeling || request.user.get('feeling');
+  var limit = request.params.limit || 1;
+
+  // Validate
+  if (!(lang in names.nouns))
+    return response.error('Invalid language');
+
+  // Nicknames
+  var adjectives = names.adjectives[lang][feeling];
+  var nouns = names.nouns[lang];
+  var nicknames = [];
+
+  // Iteration
+  for (var i = 0; i < limit; i++) {
+    var adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    var noun = nouns[Math.floor(Math.random() * nouns.length)];
+    var nickname = adjective + ' ' + noun;
+
+    // Capitalize nickname
+    nickname = nickname.replace(/(?:^|\s)\S/g, function(a) {
+      return a.toUpperCase();
+    });
+
+    nicknames.push(nickname);
+  }
+
+  response.success(nicknames);
+});
+
+/**
+ * Create a shout
+ *
+ * @todo Validate nickname and feeling
+ *
+ * @param {string} content
+ *
+ * @response {Parse.Object} shout
+ */
+Parse.Cloud.define('shout', function(request, response) {
+  Parse.Cloud.useMasterKey();
+
+  // Params
+  var content = request.params.content;
+  var user = request.user;
+
+  // Empty validations
+  if (!user)                  return response.error('Empty user');
+  if (!user.get('nickname'))  return response.error('Empty nickname');
+  if (!user.get('location'))  return response.error('Empty location');
+  if (!user.get('place'))     return response.error('Empty place');
+  if (!user.get('feeling'))   return response.error('Empty feeling');
+  if (!content)               return response.error('Empty content');
+
+  // More validations
+  if (content.length > 255) {
+    return response.error('Content should not be larger than 255 characters');
+  }
+
+  // Object
+  var shout = new Parse.Object('Shout');
+
+  // Fetch place
+  user.get('place').fetch().then(function(place) {
+    shout.set('user', user);
+    shout.set('nickname', user.get('nickname'));
+    shout.set('location', user.get('location'));
+    shout.set('place', user.get('place'));
+    shout.set('feeling', user.get('feeling'));
+    shout.set('content', content);
+    shout.set('echoes', 0);
+    shout.set('comments', 0);
+
+    return shout.save();
+  }).then(function(shout) {
+    var place = user.get('place');
+
+    place.increment('shouts');
+    place.save();
+
+    response.success();
+  }, response.error);
+});
+
+/**
+ * Comment a shout
+ *
+ * @todo Validate nickname
+ *
+ * @param {string} shoutId
+ * @param {string} content
+ *
+ * @response {Parse.Object} comment
+ */
+Parse.Cloud.define('comment', function(request, response) {
+  Parse.Cloud.useMasterKey();
+
+  // Params
+  var shoutId = request.params.should;
+  var content = request.params.content;
+  var user = request.user;
+
+  // Empty validations
+  if (!user)                  return response.error('Empty user');
+  if (!user.get('nickname'))  return response.error('Empty nickname');
+  if (!user.get('location'))  return response.error('Empty location');
+  if (!user.get('place'))     return response.error('Empty place');
+  if (!content)               return response.error('Empty content');
+
+  // More validations
+  if (content.length > 255) {
+    return response.error('Content should not be larger than 255 characters');
+  }
+
+  // Object
+  var comment = new Parse.Object('Comment');
+  var shout = new Parse.Object('Shout');
+  shout.id = shoutId;
+
+  user.get('place').fetch().then(function(place) {
+    return shout.fetch();
+  }).then(function(shout) {
+    comment.set('shout', shout);
+    comment.set('user', user);
+    comment.set('nickname', user.get('nickname'));
+    comment.set('location', user.get('location'));
+    comment.set('place', user.get('place'));
+    comment.set('content', content);
+
+    return comment.save();
+  }).then(function(comment) {
+    shout.increment('comments');
+    shout.save();
+
+    response.success(comment);
+  }, response.error);
+});
---
+++ cloud/names.js
@@ -0,0 +1,52 @@
+exports.nouns = {
+  'en': [
+    'boy', 'girl', 'man', 'woman', 'cow', 'pig', 'cat', 'dog', 'banana',
+    'king', 'queen', 'prince', 'princess', 'maid', 'mice', 'snake', 'star',
+    'animal', 'brother', 'sister', 'chicken', 'doctor', 'frog', 'horse',
+    'kitten', 'rabbit', 'spy', 'tiger', 'zebra', 'actor', 'actress', 'fish',
+    'goldfish', 'egg', 'cake', 'pancake', 'popcorn', 'apple', 'beetle', 'bee',
+    'beast', 'beggar', 'coach', 'donkey', 'finger', 'ghost', 'giraffe', 'rat',
+    'governor', 'hydrant', 'judge', 'slave', 'visitor', 'beginner', 'lover',
+    'creator', 'creature', 'fruit', 'lawyer', 'magician', 'passenger', 'goat',
+    'adventurer', 'squirrel', 'stranger', 'baby', 'bear', 'bird', 'dinosaur',
+    'doll', 'duck', 'lover', 'expert', 'spirit', 'player', 'farmer', 'juice',
+    'teacher', 'fireman', 'friend', 'giant', 'machine', 'manager', 'monkey',
+    'nordic', 'officer', 'orange', 'person', 'pet', 'pest', 'potato', 'snail',
+    'secretary', 'sheep', 'spider', 'tomato', 'toy', 'walker', 'armadillo',
+    'loser', 'catfish', 'dogfish', 'puppy', 'doggie', 'gorilla', 'camel',
+    'turtle', 'jackass', 'nerd', 'geek', 'dwarf', 'elf', 'thief', 'angel',
+    'genius', 'phantom', 'poltergeist', 'demon', 'dragon', 'academic', 'guy',
+    'student', 'mermaid', 'parrot', 'pianist', 'gamer', 'player', 'driver',
+    'traveler', 'voyager', 'rider', 'aviator', 'asian', 'latin', 'naturalist',
+    'surfer', 'sedentary', 'hunter', 'assassin', 'businessman', 'merchant',
+    'hawk', 'pirate', 'hacker', 'drunkard', 'workaholic', 'celebrity', 'lion',
+    'leopard', 'boss', 'vigilant'
+  ]
+};
+
+exports.adjectives = {
+  'en': {
+    'red': [
+      'delightful', 'beautiful', 'fascinating', 'hot', 'tasty', 'fluffy',
+      'exciting', 'sexy', 'amazing', 'captivating', 'cute', 'wet', 'juicy',
+      'delicious', 'adorable', 'glamorous', 'handsome', 'sweet', 'yummy',
+      'graceful'
+    ],
+    'green': [
+      'happy', 'great', 'funny', 'magical', 'fantastic', 'kind', 'light',
+      'wonderful', 'incredible', 'brilliant', 'friendly', 'cool', 'bracing',
+      'inspiring', 'fancy', 'powerful', 'gentle', 'victorious', 'rousing'
+    ],
+    'blue': [
+      'boring', 'annoying', 'serious', 'tedious', 'cold', 'broken', 'odd',
+      'drowsy', 'revolting', 'outrageous', 'pathetic', 'creepy', 'angry',
+      'lazy', 'obnoxious', 'offensive', 'rude', 'noisy', 'dry', 'dirty',
+      'wrong', 'salty', 'malicious', 'freezing'
+    ],
+    'black': [
+      'sad', 'depressing', 'painful', 'morbid', 'dark', 'shy', 'damaged',
+      'pitiful', 'heartbreaking', 'repulsive', 'scary', 'dead', 'tasteless',
+      'mysterious', 'helpless', 'embarrassed', 'defeated', 'weak', 'eager'
+    ]
+  }
+};
