c8505d5226018af39b5e73b44b62c22078930906
Diego Haz <hazdiego@gmail.com>
2015-04-12 12:11:41 -0300

    Define post validation for Shout and Comment

--- cloud/validations.js
+++ cloud/validations.js
@@ -19,3 +19,40 @@ exports.feeling = function(feeling) {
     return false;
   }
 }
+
+// Validate post
+exports.post = function(post, user, response) {
+  // Definitions
+  post.get('user')       || post.set('user', user);
+  post.get('nickname')   || post.set('nickname', user.get('nickname'));
+  post.get('location')   || post.set('location', user.get('location'));
+  post.get('place')      || post.set('place', user.get('place'));
+  post.get('placeTemp')  || post.set('placeTemp', user.get('placeTemp'));
+  post.get('feeling')    || post.set('feeling', user.get('feeling'));
+
+  // Place
+  var place = post.get('place') || post.get('placeTemp');
+
+  // Empty validations
+  if (!post.get('user'))     return !!response.error('Empty user');
+  if (!post.get('nickname')) return !!response.error('Empty nickname');
+  if (!post.get('location')) return !!response.error('Empty location');
+  if (!post.get('feeling'))  return !!response.error('Empty feeling');
+  if (!post.get('content'))  return !!response.error('Empty content');
+  if (!place)                return !!response.error('Empty place');
+
+  // More validations
+  if (!exports.nickname(user.get('nickname'))) {
+    return !!response.error('Invalid nickname');
+  }
+
+  if (!exports.feeling(user.get('feeling'))) {
+    return !!response.error('Invalid feeling');
+  }
+
+  if (post.get('content').length > 255) {
+    return !!response.error('Content should not be larger than 255 characters');
+  }
+
+  return true;
+}
